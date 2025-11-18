d8c414676e589428402617d9ad99568fdaee8100
Baris Gorgun <BARISGO1@intertech.com.tr>
2025-11-18 09:56:02 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Gorgun, Baris on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
